ed1280edce5ed9ee6cc2dc1a5eed3eeb3aa91a07
maria-nusrat-2005 <marianusrat.tonu34@gmail.com>
2025-07-27 22:10:53 +0600

    sign_in page done

--- app/src/main/java/com/example/snaptask_v01/MainActivity.java
+++ app/src/main/java/com/example/snaptask_v01/MainActivity.java
@@ -21,39 +21,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        bottomNav = findViewById(R.id.bottom_navigation);
-        bottomNav.setOnNavigationItemSelectedListener(navListener);
+//        bottomNav = findViewById(R.id.bottom_navigation);
+//        bottomNav.setOnNavigationItemSelectedListener(navListener);
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
 //            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 //            return insets;
 //        });
     }
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
-            item -> {
-                Fragment selectedFragment = null;
-
-                switch (item.getItemId()) {
-                    case R.id.nav_home:
-                        selectedFragment = new HomeFragment();
-                        break;
-                    case R.id.nav_post:
-                        selectedFragment = new PostFragment();
-                        break;
-                    case R.id.nav_messages:
-                        selectedFragment = new MessageFragment();
-                        break;
-                    case R.id.nav_notifications:
-                        selectedFragment = new NotificationFragment();
-                        break;
-                    case R.id.nav_profile:
-                        selectedFragment = new ProfileFragment();
-                        break;
-                }
-
-                getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.fragment_container, selectedFragment).commit();
-
-                return true;
-            };
+//    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+//            item -> {
+//                Fragment selectedFragment = null;
+//
+//                switch (item.getItemId()) {
+//                    case R.id.nav_home:
+//                        selectedFragment = new HomeFragment();
+//                        break;
+//                    case R.id.nav_post:
+//                        selectedFragment = new PostFragment();
+//                        break;
+//                    case R.id.nav_messages:
+//                        selectedFragment = new MessageFragment();
+//                        break;
+//                    case R.id.nav_notifications:
+//                        selectedFragment = new NotificationFragment();
+//                        break;
+//                    case R.id.nav_profile:
+//                        selectedFragment = new ProfileFragment();
+//                        break;
+//                }
+//
+//                getSupportFragmentManager().beginTransaction()
+//                        .replace(R.id.fragment_container, selectedFragment).commit();
+//
+//                return true;
+//            };
 }
